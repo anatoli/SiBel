f8d0a73673a649686dfe727c2685fb6930fbbacc
Anatoliy.Arinovich <Anatoliy.Arinovich@sibel.by>
2016-09-14 11:31:08 +0300

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name siBelApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the siBelApp
+ */
+angular.module('siBelApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name siBelApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the siBelApp
+ */
+angular.module('siBelApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
