d45bf9e464f4fba3152dde3e69f1f42f7c6259cd
Anatoliy.Arinovich <Anatoliy.Arinovich@sibel.by>
2016-09-28 18:06:04 +0300

    28/09

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,6 +3,21 @@
 angular.module('siBelApp')
   .controller('HomeCtrl',[ '$scope', '$uibModal', '$state', function ($scope, $uibModal, $state) {
 
+    $scope.hpSrc = "../../images/vendors/hp_white.png";
+
+    setTimeout(function () {
+      $scope.iconFirs = true;
+      $('#first-icon').addClass('flip');
+    }, 2000);
+    setTimeout(function () {
+      $scope.iconFirs = true;
+      $('#second-icon').addClass('flip');
+    }, 3000);
+    setTimeout(function () {
+      $scope.iconFirs = true;
+      $('#third-icon').addClass('flip');
+    }, 4000);
+
     $scope.by=false;
     $scope.ru=false;
     $scope.en=false;
@@ -33,4 +48,11 @@ angular.module('siBelApp')
       window.scrollTo(0, 0);
       $state.go('root.404')
     }
+    $scope.hpLeave=function () {
+      $scope.hpSrc = "../../images/vendors/hp_white.png"
+    }
+    $scope.hpMove=function () {
+      $scope.hpSrc = "../../images/vendors/hp.png"
+    }
+
   }]);
