6bd7493ec4c2fc9a8bdf2ee73aaece1c187eaef1
Anatoliy Arinovich <arinov@tut.by>
2016-10-28 16:55:52 +0300

    company blue-block

--- app/scripts/controllers/CompanyCtrl.js
+++ app/scripts/controllers/CompanyCtrl.js
@@ -6,14 +6,18 @@
  * Created by Anatoli on 16.09.2016.
  */
 angular.module('siBelApp')
-  .controller('CompanyCtrl', ['$scope', '$uibModal', '$state', '$translate', '$rootScope',
-    function ($scope, $uibModal, $state, $translate, $rootScope) {
+  .controller('CompanyCtrl', ['$scope', '$uibModal', '$state', '$translate', '$rootScope', '$location',
+    function ($scope, $uibModal, $state, $translate, $rootScope, $location) {
+
+      var arr = $location.$$path.split('/');
+      var n = arr.length-1;
+      $scope.state = "."+arr[n];
+      console.log($scope.state);
 
     $scope.Go = function (data) {
       $state.go('root.company'+data);
+      $scope.state = data;
     }
-    $scope.$watch($state, function (nv,ov) {
-    })
 
 
 
